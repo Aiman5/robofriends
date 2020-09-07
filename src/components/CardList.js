33ca3d344d662870a3728d57db43c23ce03fa9c5
Aiman5 <69766332+Aiman5@users.noreply.github.com>
2020-09-07 16:57:44 -0700

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {		
+	return (
+   <div>
+   {
+   	robots.map((user, i) => {
+      return(
+		<card 
+		key={i} 
+		id={robots[i].id} 
+		name={robots[i].name} 
+		email ={robots[i].email}
+		/>
+		);
+	})
+   }
+	
+		</div>
+
+   );
+}
+
+export default CardList;
